4d53b7e18af851d77bf0c9d367a014f2b8db5878
Jhoel0521 <jhoel0521@gmail.com>
2025-07-18 08:55:06 -0400

    feat: cambiar clave de respuesta 'user' a 'data' en el método me del AuthController

--- BackEnd/app/Http/Controllers/Auth/AuthController.php
+++ BackEnd/app/Http/Controllers/Auth/AuthController.php
@@ -97,7 +97,7 @@ class AuthController extends Controller
 
         return response()->json([
             'success' => true,
-            'user' => $user
+            'data' => $user
         ]);
     }
 
